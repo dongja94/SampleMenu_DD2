e2c265017172ce19f0c9f66225936c50e66fcb84
Yon Seungik <dongja94@gmail.com>
2016-07-26 10:28:41 +0900

    showAsAction

--- app/src/main/java/com/begentgroup/samplemenu/MainActivity.java
+++ app/src/main/java/com/begentgroup/samplemenu/MainActivity.java
@@ -1,7 +1,10 @@
 package com.begentgroup.samplemenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,6 +20,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_1);
+        SearchView view = (SearchView)MenuItemCompat.getActionView(item);
+        view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, "query : " + query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.i("MainActivity", "text : " + newText);
+                return false;
+            }
+        });
+//        View view = MenuItemCompat.getActionView(item);
+//        EditText inputView = (EditText)view.findViewById(R.id.editText);
+//        inputView.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                Log.i("MainActivity", charSequence.toString());
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+//
+//        MenuItemCompat.setOnActionExpandListener(item, new MenuItemCompat.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionExpand(MenuItem item) {
+//                Toast.makeText(MainActivity.this, "expand", Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionCollapse(MenuItem item) {
+//                Toast.makeText(MainActivity.this, "collapse", Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//        });
+//
+//        MenuItemCompat.expandActionView(item);
         return true;
     }
 
